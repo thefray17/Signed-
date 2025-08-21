837d269add2f82a4d4a591afdf8dc8f1f7b804ee
Fred Fortuito <ffortuito@gmail.com>
2025-08-21 11:09:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -107,7 +107,6 @@ export default function DashboardPage() {
     }
   }
 
-
   return (
     <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
       <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
