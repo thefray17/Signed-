51cdef036751d012fe870b931d5fd4304c3bddf1
Fred Fortuito <ffortuito@gmail.com>
2025-08-22 07:40:49 +0000

    src/hooks/use-mobile.tsx
    "use client";
    
    import * as React from "react"

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,24 +1,30 @@
 "use client";
 
+
 import * as React from "react";
 
+
 const MOBILE_BREAKPOINT = 768;
+
 
 /**
 * SSR-safe mobile breakpoint detector.
 * Why: Prevents importing this hook from a Server Component without a client boundary.
 */
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
+const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
 
-  React.useEffect(() => {
-    const onChange = () => setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
 
-    mql.addEventListener("change", onChange);
-    onChange();
-    return () => mql.removeEventListener("change", onChange);
-  }, []);
+React.useEffect(() => {
+const onChange = () => setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
 
-  return !!isMobile;
+
+mql.addEventListener("change", onChange);
+onChange();
+return () => mql.removeEventListener("change", onChange);
+}, []);
+
+
+return !!isMobile;
 }
